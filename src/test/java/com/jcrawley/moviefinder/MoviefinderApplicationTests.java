5ff17f3d1d8d8b3f2978b606fbdb1b5a48760192
johncrawley <johncrawley1@gmail.com>
2021-06-23 21:08:38 +0100

    Initial project commit

---
+++ src/test/java/com/jcrawley/moviefinder/MoviefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.moviefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
